c2762d53b5c4bf03d33bbe157ce0b297237e4533
Bryn Dyment <bryndyment@gmail.com>
2021-03-21 16:56:31 +0900

    1.1.1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-exports.useOpener = () => console.log('useOpener')
+const { useCallback, useState } = require('react')
+
+exports.useOpener = (
+  initialState = {
+    anchor: null,
+    isOpen: false
+  }
+) => {
+  const [opener, setOpener] = useState(initialState)
+
+  const close = useCallback(() => setOpener({ anchor: null, isOpen: false }), [])
+
+  const open = useCallback((event = {}) => {
+    const { currentTarget = null } = event
+
+    setOpener({ anchor: currentTarget, isOpen: true })
+  }, [])
+
+  const toggle = opener.isOpen ? close : open
+
+  return {
+    ...opener,
+    close,
+    open,
+    toggle
+  }
+}
